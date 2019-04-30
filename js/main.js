52c3560183948bf48828486e76ac9e2dd223a33d
danielagutperl <daniela.gutperl@gmail.com>
2019-04-30 13:02:58 +0100

    adding game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay =[];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+let cardOne = cards[0];
+let cardTwo = cards[1];
+let cardThree = cards[1];
+let cardFour = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!")
+} else {
+    alert("Keep looking for a match!")
+}
+}
+
+console.log("User flipped " + cardsInPlay);
